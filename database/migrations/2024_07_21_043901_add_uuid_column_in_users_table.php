3d42b89101c5db6dca23a3267bd0f88e03202cbd
WidyaX1900 <tuderangga93@gmail,com>
2024-07-21 12:50:53 +0800

    Add index in user uuid column

--- database/migrations/2024_07_21_043901_add_uuid_column_in_users_table.php
+++ database/migrations/2024_07_21_043901_add_uuid_column_in_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid()->after("id");
+            $table->uuid()->after("id")->index();
         });
     }
 
